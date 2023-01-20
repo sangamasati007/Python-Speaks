d0b1acbc5d10c237c2c9d1e11c040010cc4e901b
Sangam Asati <52316704+sangamasati007@users.noreply.github.com>
2023-01-20 11:50:34 +0530

    Day1 files
    Hey, these are the day 1 files for the learning and small brand name project that I have built.

---
+++ day1_learning.py
@@ -0,0 +1,29 @@
+#1 Printing on screen👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+print("Hello" + " " + "World")
+
+#2 Taking input form user👇
+print(len(input('What is your name? ')))
+
+#3 Variables in python
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+temp = a
+a = b
+b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
---
+++ day1_projects.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator.')
+
+user_city = input('What is your city name?\n')
+
+pet = input('What is your pet name?\n')
+
+print("Your band name could be " + user_city + " " + pet)
